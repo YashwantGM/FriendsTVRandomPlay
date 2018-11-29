dc25f12f2a385bc4a9eaddfd9c3deebad36221fb
YashwantGM <40892840+YashwantGM@users.noreply.github.com>
2018-11-29 13:25:50 +0530

    Second version

--- FriendsRandomPlayer.py
+++ FriendsRandomPlayer.py
@@ -1,19 +1,20 @@
-import subprocess
+import subprocess 
 import os
-from vlc import Instance 
-vlcInstance = vlc.Instance()
-player = vlcInstance.media_player_new()
-Media = vlcInstance.media_new('Settings 05-11-2018 16_22_02.mp4')
-Media.get_mrl()
-player.set_media(Media)
-print(media.get_mrl()) # File location to get title 
-print(player.get_length()) #Time duration of file -1 means there is no media
-print(player.get_state())
-player.play()
-# p = subprocess.Popen([os.path.join("C:/", "Program Files", "VideoLAN", "VLC", "vlc.exe")])#,
-	#os.path.join("C:/", "Users", "Developer", "Videos", "Captures", "Settings 05-11-2018 16_22_02.mp4")])
+import random
+# import vlc 
+# vlcInstance = vlc.Instance()
+# player = vlcInstance.media_player_new()
+# Media = vlcInstance.media_new('Settings 05-11-2018 16_22_02.mp4')
+# Media.get_mrl()
+# player.set_media(Media)
+# print(media.get_mrl()) # File location to get title 
+# print(player.get_length()) #Time duration of file -1 means there is no media
+# print(player.get_state())
+# player.play()
+# p = subprocess.Popen([os.path.join("C:/", "Program Files", "VideoLAN", "VLC", "vlc.exe"),
+# 	os.path.join("C:/", "Users", "Developer", "Videos", "Captures", "Settings.mp4")])
 # title = 'Settings 05-11-2018 16_22_02'
-# p = subprocess.Popen(["C:/Program Files/VideoLAN/VLC","C:/Users/Developer/Videos/Captures" + title + '.mp4'])
+#p = subprocess.Popen(["C:/Program Files/VideoLAN/VLC"])#,"C:/Users/Developer/Videos/Captures" + title + '.mp4'])
 
 # -*- coding: utf-8 -*-
 
@@ -41,3 +42,19 @@ player.play()
 # Spelar = newPlayer()
 # Spelar.Open()
 # Spelar.Play()
+
+# x = subprocess.Popen('dir', stdout=subprocess.PIPE, shell=True)
+# x.communicate(stdout)
+
+
+seasons = ['season 1','season 2','season 3','season 4','season 5','season 6']
+random_season = random.choice(seasons)
+print(random_season)
+
+episode = ['1','2','3','4','5']	
+rand_episode = random.choice(episode)
+print(rand_episode)
+
+cmd = 'vlc'+' F:\\movies\\tvshows\\F.R.I.E.N.D.S\\'+random_season+'\\'+rand_episode+'.mp4' 
+print(cmd)
+os.system(cmd)
